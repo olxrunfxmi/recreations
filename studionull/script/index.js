5aa68e22d129be609804b925c2acb8bf4ae38a66
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-02-28 17:30:04 +0100

    Create alternate html UI for work

--- studionull/script/index.js
+++ studionull/script/index.js
@@ -1,8 +1,46 @@
 const links = document.querySelectorAll(".nav-link");
+const bottomBase = document.querySelector(".bottom-base");
 
 links.forEach((link) => {
 	link.addEventListener("click", (e) => {
-        e.preventDefault();
-        
+		e.preventDefault();
+		bottomBase.classList.toggle("disappear");
 	});
 });
+
+const projects = [
+	{
+		name: "Korty",
+		details:
+			"Identity and Website for one of Nigeria's fastest rising youtubers",
+		href: "https://kortyeo.madebynull.com/",
+	},
+	{
+		name: "Paystack Terminal",
+		details: "Marketing website for Paystack’s Point of Sale solution",
+		href: "https://deploy-preview-162--paystack-terminal.netlify.app/terminal/",
+	},
+	{
+		name: "Radio Null Rewind 2022",
+		details: "A music-themed year in review website for 2022.",
+		href: "https://2022.madebynull.com/",
+	},
+	{
+		name: "Danfo",
+		details:
+			"Bespoke type specimen website for a Google fonts commissioned typeface.",
+		href: "https://www.afrotype.com/danfo",
+	},
+	{
+		name: "PITH Africa",
+		details:
+			"E-commerce and editorial experience for a lagos based fashion house",
+		href: "https://pithafrica.com/",
+	},
+	{
+		name: "Group 256",
+		details:
+			"Portfolio website for Visual Designer and Art Director, Isaac Fayemi.",
+		href: "https://www.fayemi.design/",
+	},
+];
