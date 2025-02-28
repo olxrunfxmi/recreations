fb725a9d69fed6661210d542959ee848a2da0e83
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-02-28 19:26:12 +0100

    Finalize Single Page View: Strange Build

--- studionull/script/index.js
+++ studionull/script/index.js
@@ -1,10 +1,34 @@
 const links = document.querySelectorAll(".nav-link");
 const bottomBase = document.querySelector(".bottom-base");
+let view = "info";
+
+const infoView = document.querySelector(".top-start");
+
+function generateInfoView() {
+	const old = infoView;
+
+	const sectionTop = document.querySelector(".top");
+	const container = document.querySelector(".top-start");
+
+	sectionTop.replaceChild(old, container);
+}
 
 links.forEach((link) => {
 	link.addEventListener("click", (e) => {
 		e.preventDefault();
-		bottomBase.classList.toggle("disappear");
+
+		if (link.id === "info" && view === "work") {
+			// Generate The Info View();
+			generateInfoView();
+			// Remove The Bottom Base
+			bottomBase.classList.toggle("disappear");
+			view = "info";
+		} else if (link.id === "work" && view === "info") {
+			generateProjectView();
+			// Add The Bottom Base
+			bottomBase.classList.toggle("disappear");
+			view = "work";
+		}
 	});
 });
 
@@ -44,3 +68,41 @@ const projects = [
 		href: "https://www.fayemi.design/",
 	},
 ];
+
+function generateProjectView() {
+	const sectionTop = document.querySelector(".top");
+	const container = document.querySelector(".top-start");
+
+	let newTopStart = document.createElement("div");
+	newTopStart.classList.add("top-start");
+
+	let item;
+
+	projects.forEach((project, index, array) => {
+		if (index % 2 === 0) {
+			item = document.createElement("div");
+			item.classList.add("top-start-item");
+		}
+		const article = document.createElement("article");
+		const h4 = document.createElement("h4");
+		h4.classList.add("start-header");
+		const a = document.createElement("a");
+		a.href = project["href"];
+		a.textContent = project["name"];
+		h4.appendChild(a);
+
+		const para = document.createElement("p");
+		para.textContent = project["details"];
+
+		article.appendChild(h4);
+		article.appendChild(para);
+
+		item.appendChild(article);
+
+		if (index % 2 === 1) {
+			newTopStart.appendChild(item);
+		}
+	});
+
+	sectionTop.replaceChild(newTopStart, container);
+}
