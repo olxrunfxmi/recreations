1b43808a17131703f78022d38707c972914d75d8
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-05-20 00:38:20 +0100

    Fix mobile layout issue with 'more on the subject' and 'latest' section

--- finland/script.js
+++ finland/script.js
@@ -1,7 +1,6 @@
 const stickyHeaderEl = document.querySelector(".sticky-header");
 
 window.onscroll = function () {
-	console.log(window.scrollY);
 	if (window.scrollY > 80) {
 		stickyHeaderEl.dataset.state = "active";
 	} else if (window.scrollY < 80) {
