f8dd2d0ffe6ecc2dd11eea86aa17b3ff4fd832b5
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-05-22 15:07:58 +0100

    Create full layout (responsive)

--- chatgpt/index.js
+++ chatgpt/index.js
@@ -1,8 +1,12 @@
 const input = document.querySelector(".input");
 const svgEl = document.querySelector(".icon.filled");
+const toggleBarEls = document.querySelectorAll(".toggle-menu");
+const sideBarEl = document.querySelector(".side-bar");
+const actionViewEl = sideBarEl.nextElementSibling;
+const mobileMenuEl = document.querySelector(".menu");
 
 input.addEventListener("keydown", (e) => {
-	const pattern = /\b[\w]\b|[\W]/; // Cool to learn regex, eh
+	const pattern = /\b[\w]\b|[\W]/; // Lovely that I learned regex, eh
 
 	// Seems makeshift, but it works. Wonder how it works for prod-level code
 	if (e.target.textContent.length === 0 && pattern.test(e.key)) {
@@ -12,4 +16,13 @@ input.addEventListener("keydown", (e) => {
 		e.target.dataset.contains = "empty";
 		svgEl.dataset.contains = "empty";
 	}
+});
+
+toggleBarEls.forEach((toggleBarEl) => {
+	toggleBarEl.addEventListener("click", (e) => {
+		sideBarEl.dataset.state =
+			sideBarEl.dataset.state === "out-of-view" ? "visible" : "out-of-view";
+		actionViewEl.dataset.state =
+			actionViewEl.dataset.state === "full-view" ? "half-view" : "full-view";
+	});
 });
