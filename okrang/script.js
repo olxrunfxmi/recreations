9abefc3cc23e285978f185ecf60491f08aab15fd
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-05-24 14:06:40 +0100

    Fix hero-end scrolling animation

--- okrang/script.js
+++ okrang/script.js
@@ -1,6 +1,14 @@
 const cookieButtonEl = document.querySelector("#cookie");
+const heroImagesEl = document.querySelector(".hero-end");
 
 cookieButtonEl.addEventListener("click", (e) => {
 	const cookieEl = e.currentTarget.parentElement;
 	cookieEl.remove();
 });
+
+window.onscroll = function () {
+	if (window.scrollY > 0 && window.scrollY <= 500) {
+		console.log(window.scrollY);
+		heroImagesEl.style.setProperty("--x", window.scrollY);
+	}
+};
