02516009928133e956243cc77c4b6def2a1cc0bc
olxrunfxmi <191869901+olxrunfxmi@users.noreply.github.com>
2025-05-24 20:13:11 +0100

    Add mobile and tablet view swiper for product section

--- okrang/script.js
+++ okrang/script.js
@@ -1,5 +1,7 @@
 const cookieButtonEl = document.querySelector("#cookie");
 const heroImagesEl = document.querySelector(".hero-end");
+const productFrameEl = document.querySelector(".product-frame");
+const ballEls = document.querySelectorAll(".ball");
 
 cookieButtonEl.addEventListener("click", (e) => {
 	const cookieEl = e.currentTarget.parentElement;
@@ -8,7 +10,25 @@ cookieButtonEl.addEventListener("click", (e) => {
 
 window.onscroll = function () {
 	if (window.scrollY > 0 && window.scrollY <= 500) {
-		console.log(window.scrollY);
 		heroImagesEl.style.setProperty("--x", window.scrollY);
 	}
 };
+
+productFrameEl.addEventListener("scroll", () => {
+	const firstProductEl = document.querySelector(".product");
+	const productDetails = firstProductEl.getBoundingClientRect();
+
+	const productWidth = productDetails.width;
+	const productLeft = productDetails.left;
+	const index = Math.floor(productLeft / (productWidth - 64)) * -1 - 1;
+
+	ballEls.forEach((ballEl, i) => {
+		if (i === index) {
+			ballEl.dataset.status = "active";
+		} else if (index === -1 && i === 0) {
+			ballEll.dataset.status = "active";
+		} else {
+			ballEl.dataset.status = "inactive";
+		}
+	});
+});
